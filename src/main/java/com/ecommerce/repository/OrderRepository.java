9ba36f27f3957baf9d8c4d1723bcb3b2f5d7f9e3
Affan <shaikabdulla0298@gmail.com>
2025-06-10 21:13:03 +0530

    Order repositoy was added

--- src/main/java/com/ecommerce/repository/OrderRepository.java
+++ src/main/java/com/ecommerce/repository/OrderRepository.java
@@ -1,5 +1,18 @@
 package com.ecommerce.repository;
 
-public class OrderRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.Order;
+import com.ecommerce.OrderStatus;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	@Query("SELECT o FROM orer o where o.status=:status")
+	List<Order> findByStatsus(OrderStatus status);
+
+	List<Order> findByUserId(Long userId);
 }
